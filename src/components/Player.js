0a4e33359899c662bf8a6b650d8771875a8e1cc4
Dmitry Kantin <kantindm@gmail.com>
2018-04-09 22:53:38 +0300

    add webView player

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, StyleSheet, View, WebView } from 'react-native';
+
+const styles = StyleSheet.create({
+    WebViewContainer: {
+        marginTop: 20,
+    }
+});
 
 export default class Player extends React.Component {
   render() {
     return (
-      <View>
-        <Text>Hello, I am an empty player</Text>
+      <View style={{ height: 300 }}>
+        <Text>Player</Text>
+        <WebView
+            style={ styles.WebViewContainer }
+            javaScriptEnabled={true}
+            domStorageEnabled={true}
+            source={{uri: 'https://www.youtube.com/embed/N6hVmn9FM7o' }}
+          />
       </View>
     );
   }
