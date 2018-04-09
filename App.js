8231663e0329266bf234419f3ab7b59c78b4a019
Dmitry Kantin <kantindm@gmail.com>
2018-04-09 20:38:04 +0300

    temp working state

--- App.js
+++ App.js
@@ -1,23 +1,52 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import ExampleScreen2 from './components/ExampleScreen2.js';
+// import React from 'react';
+// import { StyleSheet, Text, View } from 'react-native';
+// import ExampleScreen2 from './components/ExampleScreen2.js';
+//
+// export default class App extends React.Component {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Text>Get started</Text>
+//         <ExampleScreen2 />
+//       </View>
+//     );
+//   }
+// }
+//
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
 
-export default class App extends React.Component {
+
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+import AppReducer from './src/reducers';
+import AppWithNavigationState from './src/navigators/AppNavigator';
+import { middleware } from './src/utils/redux';
+
+const store = createStore(
+  AppReducer,
+  applyMiddleware(middleware),
+);
+
+class ReduxExampleApp extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Get started</Text>
-        <ExampleScreen2 />
-      </View>
+      <Provider store={store}>
+        <AppWithNavigationState />
+      </Provider>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+AppRegistry.registerComponent('ReduxExample', () => ReduxExampleApp);
+
+export default ReduxExampleApp;
