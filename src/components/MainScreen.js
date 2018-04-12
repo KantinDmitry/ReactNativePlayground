a6419027ccbf4ed67e40fec487116bdfcf49d0b9
Dmitry Kantin <kantindm@gmail.com>
2018-04-12 23:45:07 +0300

    reuse transition button

--- src/components/MainScreen.js
+++ src/components/MainScreen.js
@@ -3,8 +3,6 @@ import { StyleSheet, View } from 'react-native';
 
 import LoginStatusMessage from './LoginStatusMessage';
 import AuthButton from './AuthButton';
-import PlayerButton from './PlayerButton';
-import AlarmsButton from './AlarmsButton';
 import TransitionButton from './TransitionButton';
 
 const styles = StyleSheet.create({
@@ -20,8 +18,17 @@ const MainScreen = () => (
   <View style={styles.container}>
     <LoginStatusMessage />
     <AuthButton />
-    <PlayerButton />
-    <AlarmsButton />
+
+    <TransitionButton
+        screenName='Alarms'
+        title='Go to Alarms screen'
+    />
+
+    <TransitionButton
+        screenName='Player'
+        title='Go to Player screen'
+    />
+
     <TransitionButton
         screenName='Search'
         title='Go to Search screen'
