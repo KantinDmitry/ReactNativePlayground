e5335b8aaa90b6b65c09b89e0fb08b3ce948c96a
AlexanderGolub <alaraks95@gmail.com>
2018-04-15 18:47:08 +0300

    add screen header

--- src/components/AlarmConfiguration.js
+++ src/components/AlarmConfiguration.js
@@ -1,40 +1,66 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Switch } from 'react-native';
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  root: {
+    height: 200,
     backgroundColor: '#F5FCFF',
   },
-  placeholder: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  header: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingBottom: 5,
+    borderBottomWidth: 1,
+    backgroundColor: '#FFFFFF',
   },
+  alarmTime: {
+    fontSize: 26,
+  }
 });
 
 class AlarmConfiguration extends Component {
-  
-  
+
   render() {
-   return (
-    <View style={styles.container}>
-      <Text style={styles.placeholder}>
-        Alarm Configuration Screen
-      </Text>
-    </View>
-   );
+    const { alarm } = this.props;
+
+    const alarmTime = new Date(alarm.time);
+    const timeHHMM = `${alarmTime.getHours()}:${alarmTime.getMinutes()}`;
+
+    return (
+      <View style={styles.root}>
+        <View style={styles.header}>
+          <Text style={styles.alarmTime}>{timeHHMM}</Text>
+          <Switch
+            value={alarm.isEnabled}
+            onValueChange={() => this.props.toggleAlarm(alarm)}
+          />
+        </View>
+      </View>
+    );
   }
 }
 
+const mapStateToProps = (state, ownProps) => {
+  const alarmId = ownProps.navigation.state.params.alarm.id;
+  return {
+    alarm: state.alarmsData.alarms.find((alarm) => alarm.id === alarmId),
+  };
+};
+
+const mapDispatchToProps = dispatch => ({
+  toggleAlarm: (alarm) => dispatch({ type: 'TOGGLE_ALARM', payload: alarm }),
+});
+
 AlarmConfiguration.propTypes = {
+  navigation: PropTypes.object.isRequired,
 };
 
 AlarmConfiguration.navigationOptions = {
   title: 'Alarm configuration',
 };
 
-export default AlarmConfiguration;
+export default connect(mapStateToProps, mapDispatchToProps)(AlarmConfiguration);
