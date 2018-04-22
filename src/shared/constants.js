a4b932ab8154a235f3c76d7fd74b3a7381d746a0
Alexander Golub <golub.alexander.al@gmail.com>
2018-04-22 22:31:32 +0300

    connect AppLauncher with actions (#13)
    * connect AppLauncher with actions

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -1,5 +1,8 @@
 const WEEK_DAYS = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
 
+const TIME_BUFFER = 15; // buffer after enabling, where alarm can't be launched
+
 export {
     WEEK_DAYS,
+    TIME_BUFFER,
 };
