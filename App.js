4537919cefe3e5b759d931020d5e8026254546c7
AlexanderGolub <alaraks95@gmail.com>
2018-04-22 16:25:36 +0300

    pass alarmId to root component

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { AppRegistry } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import PropTypes from 'prop-types';
 
 import AppReducer from './src/reducers';
 import AppWithNavigationState from './src/navigators/AppNavigator';
@@ -14,6 +15,17 @@ const store = createStore(
 );
 
 class ReduxExampleApp extends React.Component {
+  static propTypes = {
+    // the id of the alarm responsible for the launch is injected as a prop
+    // (react-native-app-launcher)
+    alarmID: PropTypes.string,
+  }
+
+  constructor(props) {
+    super(props);
+    console.log('App.js', props);
+  }
+
   render() {
     return (
       <Provider store={store}>
