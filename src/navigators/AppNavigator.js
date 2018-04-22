3c1dc46ef01f10a266c89a89b7947f88a9779c80
Dmitry Kantin <kantindm@gmail.com>
2018-04-22 15:49:00 +0300

    remove example components

--- src/navigators/AppNavigator.js
+++ src/navigators/AppNavigator.js
@@ -3,19 +3,16 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { addNavigationHelpers, StackNavigator } from 'react-navigation';
 
-import LoginScreen from '../components/LoginScreen';
+import { addListener } from '../utils/redux';
+
 import MainScreen from '../components/MainScreen';
-import ProfileScreen from '../components/ProfileScreen';
 import Player from '../components/Player';
 import Alarms from '../components/Alarms';
 import Search from '../components/Search';
 import AlarmConfiguration from '../components/AlarmConfiguration';
-import { addListener } from '../utils/redux';
 
 export const AppNavigator = StackNavigator({
-  Login: { screen: LoginScreen },
   Main: { screen: MainScreen },
-  Profile: { screen: ProfileScreen },
   Player: { screen: Player },
   Alarms: { screen: Alarms },
   Search: { screen: Search },
