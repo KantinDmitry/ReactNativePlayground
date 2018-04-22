2b6708bf3d7588cad74869262f8038513c16f2b9
AlexanderGolub <alaraks95@gmail.com>
2018-04-22 15:06:10 +0300

    open app on alarm

--- android/app/src/main/java/com/youtubealarm/MainApplication.java
+++ android/app/src/main/java/com/youtubealarm/MainApplication.java
@@ -11,6 +11,8 @@ import com.facebook.soloader.SoLoader;
 import java.util.Arrays;
 import java.util.List;
 
+import io.cmichel.appLauncher.LauncherPackage;  // add this for react-native-app-launcher
+
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
@@ -22,7 +24,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+          new LauncherPackage()     // add this for react-native-app-launcher
       );
     }
 
