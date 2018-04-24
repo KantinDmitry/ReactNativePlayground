d87edc5667f82cfda3a5883417a20e607aba21ca
AlexanderGolub <alaraks95@gmail.com>
2018-04-24 17:20:18 +0300

    TransitionButton restyling

--- src/components/TransitionButton.js
+++ src/components/TransitionButton.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Button } from 'react-native';
+import { Button, TouchableOpacity, Text, StyleSheet } from 'react-native';
 import { NavigationActions } from 'react-navigation';
 
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: "#FF0000",
+    height: 60,
+    color: '#FFFFFF',
+    textAlign: 'center',
+    fontSize: 18,
+    fontWeight: 'bold',
+    borderRadius: 3,
+    padding: 15,
+  }
+});
+
 const TransitionButton = ({ screenName, title, goToScreen, screenParams }) => (
-  <Button
-    title={title}
-    onPress={() => goToScreen(screenName, screenParams)}
-  />
+  <TouchableOpacity
+    activeOpacity={0.6}
+    onPress={() => goToScreen(screenName, screenParams)}>
+    <Text
+      style={styles.button}
+    >
+      {title}
+    </Text> 
+  </TouchableOpacity>
 );
 
 TransitionButton.propTypes = {
