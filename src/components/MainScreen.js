fcdacb1e02404274ce3642c8da82df69388a8511
AlexanderGolub <alaraks95@gmail.com>
2018-04-24 17:19:33 +0300

    MainScreen restyling

--- src/components/MainScreen.js
+++ src/components/MainScreen.js
@@ -4,40 +4,50 @@ import { StyleSheet, View } from 'react-native';
 import TransitionButton from './TransitionButton';
 
 const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    alignItems: 'center',
+    paddingTop: 100,
+    paddingBottom: 180,
+    backgroundColor: '#FAFAFA',
+  },
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: 'space-between',
+    alignItems: 'stretch',
+    width: 200,
   },
 });
 
 const MainScreen = () => (
-  <View style={styles.container}>
-    <TransitionButton
-        screenName='Alarms'
-        title='Go to Alarms screen'
-    />
+  <View style={styles.root}>
+    <View style={styles.container}>
+      <TransitionButton
+          screenName='Alarms'
+          title='Go to Alarms screen'
+      />
 
-    <TransitionButton
-        screenName='Playlists'
-        title='Go to Playlists screen'
-    />
+      <TransitionButton
+          screenName='Playlists'
+          title='Go to Playlists screen'
+      />
 
-    <TransitionButton
-        screenName='Player'
-        title='Go to Player screen'
-    />
+      <TransitionButton
+          screenName='Player'
+          title='Go to Player screen'
+      />
 
-    <TransitionButton
-        screenName='Search'
-        title='Go to Search screen'
-    />
+      <TransitionButton
+          screenName='Search'
+          title='Go to Search screen'
+      />
+    </View>
   </View>
 );
 
 MainScreen.navigationOptions = {
-  title: 'Home Screen',
+  title: 'YouTube Alarm',
+  headerTintColor: '#FF0000',
 };
 
 export default MainScreen;
